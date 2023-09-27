1a88222df927dd96170ecea21bbc4d2728a13e02
Bruno Mariani <bruno0907@gmail.com>
2023-09-26 23:07:06 -0300

    feat: added create user feat

---
+++ api/src/users/users.service.ts
@@ -0,0 +1,53 @@
+import { Injectable, ConflictException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaService } from 'src/database/prisma.service';
+import { hash } from 'bcryptjs';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const emailAlreadyInUse = await this.prismaService.user.findUnique({
+      where: {
+        email: createUserDto.email,
+      },
+    });
+
+    if (emailAlreadyInUse) {
+      throw new ConflictException('Email already in use');
+    }
+
+    const passwordHash = await hash(createUserDto.email, 12);
+
+    const user = this.prismaService.user.create({
+      data: {
+        name: createUserDto.name,
+        email: createUserDto.email,
+        password: passwordHash,
+        categories: {
+          createMany: {
+            data: [
+              // INCOME
+              { name: 'Salário', icon: 'salary', type: 'INCOME' },
+              { name: 'Freelance', icon: 'freelance', type: 'INCOME' },
+              { name: 'Outro', icon: 'other', type: 'INCOME' },
+              // OUTCOME
+              { name: 'Casa', icon: 'home', type: 'OUTCOME' },
+              { name: 'Alimentação', icon: 'food', type: 'OUTCOME' },
+              { name: 'Educação', icon: 'education', type: 'OUTCOME' },
+              { name: 'Lazer', icon: 'fun', type: 'OUTCOME' },
+              { name: 'Mercado', icon: 'grocery', type: 'OUTCOME' },
+              { name: 'Roupas', icon: 'clothes', type: 'OUTCOME' },
+              { name: 'Transporte', icon: 'transport', type: 'OUTCOME' },
+              { name: 'Viagem', icon: 'travel', type: 'OUTCOME' },
+              { name: 'Outro', icon: 'other', type: 'OUTCOME' },
+            ],
+          },
+        },
+      },
+    });
+
+    return user;
+  }
+}
